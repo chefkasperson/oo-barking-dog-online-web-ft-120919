26f97e7bffe1e982fdb4e500a34c7dd514b16cb7
Erik Kasperson <chefkasperson@gmail.com>
2019-12-13 18:24:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,9 +4,9 @@ class Dog
     @this_dogs_name = dog_name
   end
   def name
-    @this_dogs_name    
+    @this_dogs_name
   end
-  def bark 
+  def bark
     puts "woof!"
   end
 end# Your code goes here!
